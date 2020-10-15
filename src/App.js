0ecec99d69fc145b8418a38fdb892c0f1818f02e
esmatptr <pointer.ptr@gmail.com>
2020-10-15 20:59:41 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -15,13 +15,14 @@ export default function App() {
     nextPage,
     visitPage
   ] = useRTDatabaseList("conversations/5f81e375e3e2fc177434db9b/messages", {
-    page: 1,
     limit: 5
   });
 
   return (
     <div>
       <h1>Hello Firebase Hooks</h1>
+      <button onClick={prevPage}>PREV</button>
+      <button onClick={nextPage}>NEXT</button>
       {loading ? <div>Loading...</div> : <p>{JSON.stringify(pageData)}</p>}
     </div>
   );
